e6f725b837977729e21021179dc00d592e72e109
NicholasQuirky <nicholaskerky.victoriano.cics@ust.edu.ph>
2024-08-17 00:47:06 +0800

    Uploaded base code
    Working Google Maps Platform API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+
+import "./App.css";
+
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
 function App() {
+  const position = { lat: 14.61, lng: 120.9893 };
+  const [open, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <APIProvider apiKey="AIzaSyDzldCRAiwwd05H9U-_wm9jUzUjbKZRd3A">
+      <div style={{ height: "100vh", width: "100%" }}>
+        <Map
+          defaultZoom={9}
+          defaultCenter={position}
+          mapId={"7e2addf355611718"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"orange"}
+              borderColor={"blue"}
+              glyphColor={"yellow"}
+            ></Pin>
+          </AdvancedMarker>
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>I'm in UST WOW I'm a tiger</p>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
   );
 }
 
